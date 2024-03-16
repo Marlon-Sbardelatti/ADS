3676a0a3f0b89e725973d407ed5ed1873371b827
HetzWGA <hetzwga@pop-os.localdomain>
2024-03-16 17:30:22 -0300

    trabalho 01

--- trabalho-01/src/Main.java
+++ trabalho-01/src/Main.java
@@ -7,15 +7,15 @@ public class Main {
         new Main();
     }
 
-    public Main(){
+    public Main() {
         Scanner kb = new Scanner(System.in);
         Queue<Tarefa> tarefas = new LinkedList<>();
 
         System.out.println("(1) - Cadastrar\n(2) - Concluir tarefa\n(3) - Visualizar próxima tarefa\n(4) - Listar\n(5) - Finalizar\n");
         int res = kb.nextInt();
 
-        while (res != 5){
-            switch (res){
+        while (res != 5) {
+            switch (res) {
                 case 1:
                     adicionar(tarefas, kb);
                     break;
@@ -40,14 +40,14 @@ public class Main {
         kb.close();
     }
 
-    private boolean adicionar(Queue<Tarefa> tarefas, Scanner kb){
+    private boolean adicionar(Queue<Tarefa> tarefas, Scanner kb) {
         System.out.println("Nome da tarefa: ");
         String nome = kb.next();
         System.out.println("Dificuldade da tarefa: ");
         String dificuldade = kb.next();
 
         Tarefa t = new Tarefa(nome, dificuldade);
-        if (!find(tarefas, t.getNome())){
+        if (!find(tarefas, t.getNome())) {
             tarefas.add(t);
             System.out.println("Tarefa adicionado com sucesso!");
             return true;
@@ -57,41 +57,42 @@ public class Main {
         return false;
     }
 
-   private boolean find(Queue<Tarefa> tarefas, String nome) {
-        for (Tarefa t : tarefas){
-            if (t.getNome().equalsIgnoreCase(nome)){
+    private boolean find(Queue<Tarefa> tarefas, String nome) {
+        for (Tarefa t : tarefas) {
+            if (t.getNome().equalsIgnoreCase(nome)) {
                 return true;
             }
         }
 
         return false;
-   }
+    }
 
-   private void listar(Queue<Tarefa> tarefas){
-       for (Tarefa t : tarefas){
-           System.out.println(t.toString() + "\n");
-       }
-   }
-
-   private boolean concluir(Queue<Tarefa> tarefas){
-        if (tarefas.remove() != null){
-            System.out.println("Tarefa concluida com sucesso!");
-            return true;
+    private void listar(Queue<Tarefa> tarefas) {
+        for (Tarefa t : tarefas) {
+            System.out.println(t.toString() + "\n");
         }
-       System.out.println("Não foi possível concluir a tarefa.");
-        return false;
-   }
+    }
 
-   private boolean showNext(Queue<Tarefa> tarefas){
+    private boolean concluir(Queue<Tarefa> tarefas) {
+        if (tarefas.isEmpty()) {
+            System.out.println("Não foi possível concluir a tarefa.");
+            return false;
+        }
+        tarefas.remove();
+        System.out.println("Tarefa concluida com sucesso!");
+        return true;
+    }
+
+    private boolean showNext(Queue<Tarefa> tarefas) {
         Tarefa next = tarefas.peek();
 
-        if (next != null){
+        if (next != null) {
             System.out.println(next.toString());
             return true;
         }
 
-       System.out.println("Sem tarefas na lista.");
-       return false;
-   }
+        System.out.println("Sem tarefas na lista.");
+        return false;
+    }
 
 }
